56e42a4e29908e6f5f173e0bcc881ea4a016ccc6
AugustusLZJ <AugustusLZJ@gmail.com>
2017-12-30 23:59:38 -0500

    lesson-7

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const routes = require('./routes/api.js');
+const bodyParser = require('body-parser');
 
 // set up express app
 const app = express();
-app.use('/api', require('./routes/api.js'));
 
 // listen for request
-app.listen(process.env.port || 4000, function() {
-   console.log('now listening for requests');  
-});
+app.use(bodyParser.json())
+   .use('/api', require('./routes/api.js'))
+   .listen(process.env.port || 4000, function() {
+        console.log('now listening for requests');  
+    });
+
